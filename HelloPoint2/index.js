29aaaedea2584696a78c3fa3d514c4593135d91c
zhuqd <1132154046@qq.com>
2020-11-11 15:37:13 +0800

    🎨  attibute uniform

--- HelloPoint2/index.js
+++ HelloPoint2/index.js
@@ -1,16 +1,21 @@
 function main() {
   const VSHADER_SOURCE = `
-    attribute vec4 a_Position
+    attribute vec4 a_Position;
+    attribute float a_PointSize;
 
     void main() {
       gl_Position = a_Position;
-      gl_PointSize = 10.0;
+      gl_PointSize = a_PointSize;
     }
     `
 
+  // precision mediump float 指定精度
   const FSHADER_SOURCE = `
+    precision mediump float;
+    uniform vec4 u_FragColor;
+
       void main(){
-        gl_FragColor = vec4(1.0, 1.0, 0.0, 1.0);
+        gl_FragColor = u_FragColor;
       }
     `
 
@@ -30,10 +35,21 @@ function main() {
   gl.clear(gl.COLOR_BUFFER_BIT)
 
   // 获取存储变量的位置
-  const aPosition = gl.getAttribLocation(program, 'a_Position')
+  const aPosition = gl.getAttribLocation(gl.program, 'a_Position')
+
+  const aPointSize = gl.getAttribLocation(gl.program, 'a_PointSize')
+
+  const aFragColor = gl.getUniformLocation(gl.program, 'u_FragColor')
+
+  if (aPosition < 0 || aPointSize < 0 || aFragColor < 0) {
+    console.log('Failed to get storge position')
+  }
 
   //
   gl.vertexAttrib3f(aPosition, 0.5, 0.5, 0.0)
+  gl.vertexAttrib1f(aPointSize, 30.0)
+
+  gl.uniform4f(aFragColor, 0.0, 0.0, 1.0, 1.0)
 
   gl.drawArrays(gl.POINTS, 0, 1)
 }
